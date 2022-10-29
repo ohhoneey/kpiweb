f921ce52167485b24aefaea68828164ccf9502aa
ohhoneey <57149528+ohhoneey@users.noreply.github.com>
2022-10-29 18:51:25 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+function manipulateImage(flag){
+    let imageToUpdate = document.getElementById("scalableImage");
+    if (flag === 1){
+        imageToUpdate.src = "https://kpi.ua/files/images-page/kyiv.jpg";
+    } else if (flag === 2){
+        imageToUpdate.style.transform = 'scale(1.1)';
+        imageToUpdate.style.transition = 'transform 0.25s ease';
+    } else if (flag === 3){
+        imageToUpdate.style.transform = 'scale(0.8)';
+        imageToUpdate.style.transition = 'transform 0.25s ease';
+    }
+    if (flag === 4){
+        imageToUpdate.src = "";
+    }
+}
+
+function changeColor(){
+    let elementToChange = document.getElementById("firstDynamicElement");
+    let elementStyle = window.getComputedStyle(elementToChange, "")
+    if (elementStyle.color === "rgb(0, 0, 0)") {
+        elementToChange.style.setProperty("color", "white");
+        elementToChange.style.setProperty("background-color", "blue")
+    } else {
+        elementToChange.style.setProperty("color", "black");
+        elementToChange.style.setProperty("background-color", "white")
+    }
+}
+
+function changeColorWithQuery(){
+    let x = document.querySelectorAll("p");
+    let elementToChange = x[3];
+    let elementStyle = window.getComputedStyle(elementToChange, "")
+    if (elementStyle.color === "rgb(0, 0, 0)") {
+        elementToChange.style.setProperty("color", "white");
+        elementToChange.style.setProperty("background-color", "green")
+    } else {
+        elementToChange.style.setProperty("color", "black");
+        elementToChange.style.setProperty("background-color", "white")
+    }
+}
